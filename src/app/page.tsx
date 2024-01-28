3316473bc5ba9e241fb486b56a33038154443c30
Joseph <joseph.clemente@hacktiv.io>
2024-01-28 20:08:40 +0800

    change

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
         <main>
             <div>
                 <Image
-                    src="../banner.jpg"
+                    src="./banner.jpg"
                     alt="Banner Image"
                     layout="fill"
                     objectFit="cover"
@@ -13,7 +13,8 @@ export default function Home() {
                 <div className="absolute inset-0 flex items-center justify-end text-right">
                     <div className="px-32">
                         <p className="text-6xl font-semibold text-modern-rich">Kimmy & Joseph</p>
-                        <p className="text-2xl text-modern-text">Kimmy & Joseph say 'I do' on [Wedding Date]!<br/> Save the date! 💍🎉
+                        <p className="text-2xl text-modern-text">Kimmy & Joseph say 'I do' on [Wedding Date]!<br/>
+                            Save the date! 💍🎉
                         </p>
                         <p className="text-1xl text-modern-text">#KimmyAndJosephSayIDo"</p>
                     </div>
