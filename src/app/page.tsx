1506e0fd3f93a2536950b97e2dab0bff5318f03f
Joseph <joseph.clemente@hacktiv.io>
2024-01-28 19:59:12 +0800

    check

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <main>
-      <div className='min-h-screen w-full bg-red-500 flex justify-center items-center'>
-        <h1 className='text-center text-green-500'>123</h1>
-      </div>
-    </main>
-  )
+    return (
+        <main>
+            <div>
+                <Image
+                    src="../banner.jpg"
+                    alt="Banner Image"
+                    layout="fill"
+                    objectFit="cover"
+                    objectPosition="center"/>
+                <div className="absolute inset-0 flex items-center justify-end text-right">
+                    <div className="px-32">
+                        <p className="text-6xl font-semibold text-modern-rich">Kimmy & Joseph</p>
+                        <p className="text-2xl text-modern-text">Kimmy & Joseph say 'I do' on [Wedding Date]!<br/> Save the date! 💍🎉
+                        </p>
+                        <p className="text-1xl text-modern-text">#KimmyAndJosephSayIDo"</p>
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
 }
